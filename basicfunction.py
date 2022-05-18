6daa2788b98c3019422ae74339f987be44601f81
monirpust <mdmonir1997@gmail.com>
2022-05-18 23:29:00 +0600

    basic processing of image gray blur canny or edge dialation erosion

---
+++ basicfunction.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+print("Some basic functions of openCV")
+
+img = cv2.imread("resources/sunflower.jpg")
+kernel = np.ones((5,5), np.uint8)
+
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(img, (7,7), 0)
+imgCanny = cv2.Canny(img, 150, 200)
+imgDialation = cv2.dilate(imgCanny, kernel, iterations=1)
+imgEroded = cv2.erode(imgDialation, kernel, iterations=1)
+
+cv2.imshow("Original Image", img)
+cv2.imshow("Gray Image", imgGray)
+cv2.imshow("Blur Image", imgBlur)
+cv2.imshow("Canny Image", imgCanny)
+cv2.imshow("Dialated Image", imgDialation)
+cv2.imshow("Eroded Image", imgDialation)
+cv2.waitKey(0)
