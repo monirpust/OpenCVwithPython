c6c875b9a6925153a49767c5a3770ed47bb448f9
monirpust <mdmonir1997@gmail.com>
2022-05-18 22:27:57 +0600

    reading image

--- chapter1.py
+++ chapter1.py
@@ -1,10 +1,15 @@
 import cv2
 print("Package imported")
 
-cap = cv2.VideoCapture("resources/minionpaper.mp4")
+img = cv2.imread("resources/monk.jpg")
 
-while True:
-    success, img = cap.read()
-    cv2.imshow("video", img)
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        break
+cv2.imshow("output", img)
+cv2.waitKey(0) # 0 for infinite wait
+
+# cap = cv2.VideoCapture("resources/minionpaper.mp4")
+
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("video", img)
+#     if cv2.waitKey(1) & 0xFF ==ord('q'):
+#         break
